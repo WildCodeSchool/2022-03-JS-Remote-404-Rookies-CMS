5871b501d9c5959ca480e33eea62daae73e88330
KevinLavigne <lavigne.kevin02@gmail.com>
2022-05-31 18:48:56 +0200

    creation du composant module 5E

--- frontend/src/pages/Page1.jsx
+++ frontend/src/pages/Page1.jsx
@@ -1,3 +1,11 @@
+import jason from "../data/generated";
+import Module5E from "../components/Module5E";
+
 export default function Page1() {
-  return <h1>acceuil</h1>;
+  return (
+    <div>
+      <h1>acceuil</h1>
+      <Module5E {...jason} />
+    </div>
+  );
 }

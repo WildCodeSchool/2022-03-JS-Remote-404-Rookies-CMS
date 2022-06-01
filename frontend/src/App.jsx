572664f9f1877168f7a5d75e626a00384227ca9d
Baasile <mathieu.basile@gmail.com>
2022-06-01 17:09:08 +0200

    create module8E

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Page2 from "./pages/Page2";
 import Page3 from "./pages/Page3";
 import Page4 from "./pages/Page4";
 import Navbar from "./components/Counter";
+// eslint-disable-next-line import/extensions
+import Module8E from "./components/Module8E";
 
 import "./App.css";
 
@@ -14,6 +16,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Page1 />} />
+        <Route path="/module8E" element={<Module8E />} />
         <Route path="/page2" element={<Page2 />} />
         <Route path="/page3" element={<Page3 />} />
         <Route path="/Page4" element={<Page4 />} />

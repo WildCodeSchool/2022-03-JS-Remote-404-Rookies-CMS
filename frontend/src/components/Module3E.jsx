cda54bb3d2aa6bc8071e4710a97b326f65f8875f
Baasile <mathieu.basile@gmail.com>
2022-06-01 14:18:55 +0200

    use json file in component

--- frontend/src/components/Module3E.jsx
+++ frontend/src/components/Module3E.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import image from "../assets/image3E.png";
 import checkbox from "../assets/Checkbox.png";
+import Jason from "../data/jason.json";
 
 function Module3E() {
   return (
@@ -9,32 +10,20 @@ function Module3E() {
         <div className="lg:max-w-lg mr-4 lg:w-full md:w-1/2 w-5/6 ">
           <img className="object-cover object-center" src={image} alt="hero" />
         </div>
-        <div className="lg:flex md:w-1/2 lg:pr-32 md:pr-16 flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h3 className="text-green-500 font-bold">
-            What is a Rookies Project?
-          </h3>
+        <div className="lg:flex md:w-1/2 lg:pr-32 md:pr-16 flex-col md:items-start md:text-left mb-16 md:mb-0 items-center      text-center">
+          <h3 className="text-green-500 font-bold">{Jason[0].secondline}</h3>
           <h2 className="text-3xl mb-4 font-bold text-gray-900">
-            A win-win collaboration
+            {Jason[0].headline}
           </h2>
-          <p className=" mb-8 leading-relaxed">
-            Submit an assignment specific to your company, taken on by higher
-            education students. The projects are carried out remotely or in
-            person as part of the students’ curriculum with the support of a
-            qualified supervisor.
-          </p>
+          <p className=" mb-8 leading-relaxed">{Jason[0].text}</p>
           <div className=" mb-2 flex">
             <img className=" mr-2" src={checkbox} alt="hero" />
-            <p className="text-sm space-x-4">
-              Students acquire resume worthy experience and connect with
-              employers
-            </p>
+            <p className="text-sm space-x-4">{Jason[0].tickphrase1}</p>
           </div>
 
           <div className="mb-8 flex">
             <img className="mr-2" src={checkbox} alt="hero" />
-            <p className="text-sm space-x-4">
-              Companies get answers to their challenges and discover talents
-            </p>
+            <p className="text-sm space-x-4">{Jason[0].tickphrase2}</p>
           </div>
 
           <button

80ab6bda2c5b31939e2bf4fe5907422d2c0a77f3
KevinLavigne <lavigne.kevin02@gmail.com>
2022-06-02 16:19:15 +0200

    module10E done

--- frontend/src/pages/Page1.jsx
+++ frontend/src/pages/Page1.jsx
@@ -1,3 +1,11 @@
+import Module10E from "@components/Module10E";
+import jason from "../data/generated";
+
 export default function Page1() {
-  return <h1>acceuil</h1>;
+  return (
+    <div>
+      <h1>acceuil</h1>
+      <Module10E {...jason} />
+    </div>
+  );
 }

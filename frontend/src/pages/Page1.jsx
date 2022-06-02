bf86e6641d48a813a50ed0ba21948ec9643bf522
KevinLavigne <lavigne.kevin02@gmail.com>
2022-06-02 15:30:50 +0200

    creation du module7E carrousel a revoir une fois contenu definitif mis en place

--- frontend/src/pages/Page1.jsx
+++ frontend/src/pages/Page1.jsx
@@ -1,3 +1,10 @@
+import Module7E from "../components/module7E";
+
 export default function Page1() {
-  return <h1>acceuil</h1>;
+  return (
+    <div>
+      <h1>acceuil</h1>
+      <Module7E />
+    </div>
+  );
 }

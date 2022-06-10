51bd3ad6199f0490bed31fa3ce75797087537d7b
GUILLAUME-GUERIT <guillaumeguerit@gmail.com>
2022-06-10 12:16:24 +0200

    fixed form and quote font

--- frontend/src/components/Quote.jsx
+++ frontend/src/components/Quote.jsx
@@ -9,7 +9,7 @@ function Quote() {
   return (
     <div className="flex flex-col items-center justify-center w-9/12">
       <div className="flex flex-col items-center justify-center w-full mb-16">
-        <p className="text-gray-900 font-bold mb-2 text-3xl italic font-mono">
+        <p className="text-gray-900 font-bold mb-2 text-2xl italic font-mono">
           &quot;{quote[entier].quote}&quot;
         </p>
       </div>

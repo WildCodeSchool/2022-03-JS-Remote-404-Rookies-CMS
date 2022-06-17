e29645756254fdf5bbd0044e012a054215feead0
KevinLavigne <lavigne.kevin02@gmail.com>
2022-06-17 14:04:11 +0200

    aujout import composant page 2

--- frontend/src/pages/Page2.jsx
+++ frontend/src/pages/Page2.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 
 import Module1S from "../components/Module1S";
+import Module2S from "../components/Module2S";
+import Module1E from "../components/Module1E";
+import Module2E from "../components/Module2E";
+import Module3E from "../components/Module3E";
+import Module7E from "../components/module7E";
+import Module9E from "../components/Module9E";
+import Footer from "../components/Footer";
 
 export default function Page2() {
   return (
     <div>
-      <h2>Page 2</h2>
+      <Module1E />
+      <Module2E />
       <Module1S />
+      <Module3E />
+      <Module7E />
+      <Module9E />
+      <Module2S />
+      <Footer />
     </div>
   );
 }
